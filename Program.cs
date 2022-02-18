aaa568b3ac40dab55b12732162115b66c3f4a528
Nirav-12345 <Nirav-12345>
2022-02-18 09:10:33 -0800

    adding Gritings Employee

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeWage_Greet;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Employee Wage Problem");
+
+    }
+}
